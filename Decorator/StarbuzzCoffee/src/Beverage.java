8b42bdd35240afed281bbcbd07d1851100a379c8
Abdulkadir98 <ak98.narutokun@gmail.com>
2018-07-06 13:34:31 +0530

    Added Decorator pattern

---
+++ Decorator/StarbuzzCoffee/src/Beverage.java
@@ -0,0 +1,12 @@
+
+public abstract class Beverage {
+	String description = "Unknown description";
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public abstract double cost();
+	
+
+}
---
+++ Decorator/StarbuzzCoffee/src/StarBuzzCoffee.java
@@ -0,0 +1,19 @@
+
+public class StarBuzzCoffee {
+
+	public static void main(String[] args) {
+		
+		Beverage beverage = new Espresso();
+		System.out.println(beverage.cost() + " " + beverage.getDescription());
+		
+		Beverage beverage2 = new HouseBlend();
+		beverage2 = new Mocha(beverage2);
+		beverage2 = new Soy(beverage2);
+		beverage2 = new Whip(beverage2);
+		
+		System.out.println(beverage2.cost() + " " + beverage2.getDescription());
+
+		
+	}
+
+}
