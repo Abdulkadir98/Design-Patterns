2b9a96b1b59c8746d4f148adf9a72c8bc827908a
Abdulkadir98 <ak98.narutokun@gmail.com>
2018-09-05 22:01:20 +0530

    Added pattern Adapter

---
+++ Adapter/Object Adapter/src/Duck.java
@@ -0,0 +1,6 @@
+public interface Duck {
+    public void fly();
+    public void quack();
+
+
+}
---
+++ Adapter/Object Adapter/src/DuckTestDrive.java
@@ -0,0 +1,23 @@
+public class DuckTestDrive {
+    public static void main(String[] args){
+        MallardDuck duck = new MallardDuck();
+        WildTurkey turkey = new WildTurkey();
+
+        Duck turkeyAdapter = new TurkeyAdapter(turkey);
+
+        System.out.println("The turkey says...");
+        turkey.gobble();
+        turkey.fly();
+
+        System.out.println("\nThe Duck says");
+        testDuck(duck);
+
+        System.out.println("The turkey adapter says..");
+        testDuck(turkeyAdapter);
+    }
+
+    static void testDuck(Duck duck){
+        duck.quack();
+        duck.fly();
+    }
+}
