944a5b4f0c71cda7820f3e3af41ade9c66840918
Abdulkadir98 <ak98.narutokun@gmail.com>
2018-09-29 22:07:21 +0530

    Used Iterator interface provided by Java's API

--- Iterator/src/DinerMenu.java
+++ Iterator/src/DinerMenu.java
@@ -1,4 +1,6 @@
-public class DinerMenu {
+import java.util.Iterator;
+
+public class DinerMenu implements Menu{
 
     static final int MAX_ITEMS = 5;
     int numberOfItems = 0;
--- Iterator/src/DinerMenuIterator.java
+++ Iterator/src/DinerMenuIterator.java
@@ -1,3 +1,6 @@
+import java.util.Iterator;
+import java.util.function.Consumer;
+
 public class DinerMenuIterator implements Iterator {
     MenuItem[] items;
     int position = 0;
@@ -19,5 +22,21 @@ public class DinerMenuIterator implements Iterator {
         MenuItem item = items[position];
         position+=1;
         return item;
+    }
+
+    @Override
+    public void remove() {
+        if(position <= 0)
+            throw  new UnsupportedOperationException("You can't remove item until you've performed at least one next");
+        if(items[position-1] != null) {
+            for(int i = position-1; i<(items.length-1); i++)
+                items[i] = items[i+1];
+            items[items.length-1] = null;
+        }
+    }
+
+    @Override
+    public void forEachRemaining(Consumer action) {
+
     }
 }
